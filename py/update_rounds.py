c86675c73d54ace0e55e3ddfbfb7b7bbeca208ad
Abraham Epton <abraham.epton@gmail.com>
2014-05-26 18:16:13 -0500

    Integrating with boto

--- py/update_rounds.py
+++ py/update_rounds.py
@@ -1,1 +1,10 @@
-import requests
+from boto.s3.connection import S3Connection
+import os
+
+conn = S3Connection(
+    os.environ.get('ABE_AWS_ACCESS_KEY_ID'), os.environ.get('ABE_AWS_SECRET_ACCESS_KEY'))
+
+print conn
+
+mybucket = conn.get_bucket('myflo.ws')
+print mybucket.list()
